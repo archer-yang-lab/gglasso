6bc012650309d9418ed749154863db7715f19347
Yi Yang <emeryyi@gmail.com>
2014-04-11 13:55:50 -0500

    1. use Hastie's fix

--- R/tools.R
+++ R/tools.R
@@ -7,15 +7,8 @@ coef.gglasso <- function(object, s = NULL, ...) {
         dimnames(nbeta) <- list(NULL, NULL)
         lambda <- object$lambda
         lamlist <- lambda.interp(lambda, s)
-        if(length(s) == 1)
-		{
-			nbeta = nbeta[, lamlist$left, drop=FALSE] * lamlist$frac +
-			nbeta[, lamlist$right, drop=FALSE] * (1 - lamlist$frac)
-		} else
-		{
-			nbeta = nbeta[, lamlist$left, drop=FALSE] %*% diag(lamlist$frac) +
-			nbeta[, lamlist$right, drop=FALSE] %*% diag(1 - lamlist$frac)
-		}
+        nbeta=nbeta[,lamlist$left,drop=FALSE]%*%Diagonal(x=lamlist$frac) 
+				+nbeta[,lamlist$right,drop=FALSE]%*%Diagonal(x=1-lamlist$frac)
         dimnames(nbeta) <- list(vnames, paste(seq(along = s)))
     }
     return(nbeta)
@@ -34,15 +27,8 @@ predict.gglasso <- function(object, newx, s = NULL, type = c("class",
         dimnames(nbeta) <- list(NULL, NULL)
         lambda <- object$lambda
         lamlist <- lambda.interp(lambda, s)
-        if(length(s) == 1)
-		{
-			nbeta = nbeta[, lamlist$left, drop=FALSE] * lamlist$frac +
-			nbeta[, lamlist$right, drop=FALSE] * (1 - lamlist$frac)
-		} else
-		{
-			nbeta = nbeta[, lamlist$left, drop=FALSE] %*% diag(lamlist$frac) +
-			nbeta[, lamlist$right, drop=FALSE] %*% diag(1 - lamlist$frac)
-		}
+        nbeta=nbeta[,lamlist$left,drop=FALSE]%*%Diagonal(x=lamlist$frac) 
+				+nbeta[,lamlist$right,drop=FALSE]%*%Diagonal(x=1-lamlist$frac)
         dimnames(nbeta) <- list(vnames, paste(seq(along = s)))
     }
     if (is.null(dim(newx))) newx = matrix(newx, nrow = 1)
